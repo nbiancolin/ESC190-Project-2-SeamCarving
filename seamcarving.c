37469ed8493c52ae80b8f07d093b20679dea1e01
nbiancolin <117390343+nbiancolin@users.noreply.github.com>
2023-03-22 17:57:29 -0400

    tiny bit mro setup stuff (mostly just to bounce to pc)

--- seamcarving.c
+++ seamcarving.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "c_img.h"
 
-typedef struct rgb_img {
-    int width;
-    int height;
-    int *data;
-} rgb_img;
 
-//Hassan I forgot we actually need a way to read in images and stuff, can you figure out how to do that?
-//Once you get that up and going lmk and I'll knock out part 1
 
 
 void calc_energy(struct rgb_img *im, struct rgb_img **grad) {
-    //printf("calc_energy");
-    int width = im -> width;
-    int height = im -> height;
+
 
     int Rx, Gx, Bx, Ry, Gy, By; //The difference in RGB values in the pixels above & below, and left & right
 
@@ -25,12 +17,17 @@ void calc_energy(struct rgb_img *im, struct rgb_img **grad) {
 
 
 
-
-    **grad = (rgb_img) {width, height, malloc(sizeof(int) * width * height)};
-
 }
 
+int main() {
+    printf("Hello, World!");
 
+    struct rgb_img *im;
+    create_img(&im, 3, 3); // do these values need to be hardcoded?
+
+
+
+}
 
 //
 // Created by Nicholas on 2023-03-22.
