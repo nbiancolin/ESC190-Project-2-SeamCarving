b886105c1e8c9ffb844ee0812861832d6831cd42
nbiancolin <117390343+nbiancolin@users.noreply.github.com>
2023-03-24 10:47:34 -0400

    Part 1 works

--- cmake-build-debug/CMakeFiles/ESC190_Project_2_SeamCarving.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ESC190_Project_2_SeamCarving.
-# This may be replaced when dependencies are built.
--- seamcarving.c
+++ seamcarving.c
@@ -9,20 +9,41 @@
 void calc_energy(struct rgb_img *im, struct rgb_img **grad) {
 
     read_in_img(&im, "../3x4.bin");
+    int width = im->width;
+    int height = im->height;
 
-    for(int i = 0; i < im->width; i++){
-        for(int j = 0; j < im->height; i++){
-
-            int Rs[5], Gs[5], Bs[5];
+    for(int i = 0; i < width; i++){
+        for(int j = 0; j < height; j++){
 
             /**
+             * New method of storing RGB: By pixel lmao
+             * 0th is Red
+             * 1st is Green
+             * 2nd is Blue
+             */
+
+             int pix[3], pixUp[3], pixDown[3], pixLeft[3], pixRight[3];
+                 for (int k = 0; k < 3; k++) {
+                     pix[k] = get_pixel(im, i, j, k);
+                     pixUp[k] = get_pixel(im, i - 1, j, k);
+                     pixDown[k] = get_pixel(im, i + 1, j, k);
+                     pixLeft[k] = get_pixel(im, i, j - 1, k);
+                     pixRight[k] = get_pixel(im, i, j + 1, k);
+                 }
+
+
+
+            /*
+            int Rs[5], Gs[5], Bs[5];
+
+
              * Layout of colour Arrays
              * 0 - Current Pixel
              * 1 - Pixel Above
              * 2 - Pixel Below
              * 3 - Pixel To the Left
              * 4 - Pixel to the Right
-             */
+
             Rs[0] = get_pixel(im, i,j,0); //Not totally sure it's important
             Rs[1] = get_pixel(im, i-1, j, 0);
             Rs[2] = get_pixel(im, i+1, j, 0);
@@ -40,10 +61,12 @@ void calc_energy(struct rgb_img *im, struct rgb_img **grad) {
             Bs[2] = get_pixel(im, i+1, j, 2);
             Bs[3] = get_pixel(im, i, j-1, 2);
             Bs[4] = get_pixel(im, i, j+1, 2);
+            */
 
             //calculate deltas
             int Rx, Gx, Bx, Ry, Gy, By; //The difference in RGB values in the pixels above & below, and left & right
 
+            /*
             Rx = Rs[2] - Rs[1];
             Gx = Gs[2] - Gs[1];
             Bx = Bs[2] - Bs[1];
@@ -51,8 +74,16 @@ void calc_energy(struct rgb_img *im, struct rgb_img **grad) {
             Ry = Rs[4] - Rs[3];
             Gy = Gs[4] - Gs[3];
             By = Bs[4] - Bs[3];
+            */
 
+            Rx = pixDown[0] - pixUp[0];
+            Gx = pixDown[1] - pixUp[1];
+            Bx = pixDown[2] - pixUp[2];
 
+            Ry = pixRight[0] - pixLeft[0];
+            Gy = pixRight[1] - pixLeft[1];
+            By = pixRight[2] - pixLeft[2];
+            
 
             //calculate energy
             int deltaX = Rx*Rx + Gx*Gx + Bx*Bx;
@@ -73,8 +104,9 @@ int main() {
     struct rgb_img *im;
     create_img(&im, 3, 4); // do these values need to be hardcoded?
 
+    struct rgb_img *grad;
 
-    struct rgb_img *grad = malloc(sizeof(struct rgb_img));
+    create_img(&grad, 3, 4);
 
     calc_energy(im,  &grad);
     printf("tempy");
