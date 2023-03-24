78abf02e55e955aff6746353a7bf9a17088a9063
nbiancolin <117390343+nbiancolin@users.noreply.github.com>
2023-03-23 21:47:10 -0400

    Bouncing to pc

--- seamcarving.c
+++ seamcarving.c
@@ -8,6 +8,75 @@
 
 void calc_energy(struct rgb_img *im, struct rgb_img **grad) {
 
+    read_in_img(&im, "3x4.bin");
+
+
+    for(int i = 0; i < im->width; i++){
+        for(int j = 0; j <im->height; i++){
+
+            int Rs[5], Gs[5], Bs[5];
+            /*
+             * Layout of colour Arrays
+             * 0 - Current Pixel
+             * 1 - Pixel Above
+             * 2 - Pixel Below
+             * 3 - Pixel To the Left
+             * 4 - Pixel to the Right
+             */
+            Rs[0] = get_pixel(im, i,j,0); //Not totally sure it's important
+            Rs[1] = get_pixel(im, i-1, j, 0);
+            Rs[2] = get_pixel(im, i+1, j, 0);
+            Rs[3] = get_pixel(im, i, j-1, 0);
+            Rs[4] = get_pixel(im, i, j+1, 0);
+
+            Gs[0] = get_pixel(im, i,j,0); //Not totally sure it's important
+            Gs[1] = get_pixel(im, i-1, j, 1);
+            Gs[2] = get_pixel(im, i+1, j, 1);
+            Gs[3] = get_pixel(im, i, j-1, 1);
+            Gs[4] = get_pixel(im, i, j+1, 1);
+
+            Bs[0] = get_pixel(im, i,j,2); //Not totally sure it's important
+            Bs[1] = get_pixel(im, i-1, j, 2);
+            Bs[2] = get_pixel(im, i+1, j, 2);
+            Bs[3] = get_pixel(im, i, j-1, 2);
+            Bs[4] = get_pixel(im, i, j+1, 2);
+
+
+            /*
+            uint8_t pix = get_pixel(im, i,j,0); //Not totally sure it's important
+            uint8_t pixUp = get_pixel(im, i+1, j, 0);
+            uint8_t pixDown = get_pixel(im, i-1, j, 0);
+            uint8_t pixLeft = get_pixel(im, i, j-1, 0);
+            uint8_t pixRight = get_pixel(im, i, j+1, 0); */
+            //Get current pixels
+            //get surrounding pixels
+
+            //calculate deltas
+            int Rx, Gx, Bx, Ry, Gy, By; //The difference in RGB values in the pixels above & below, and left & right
+
+            Rx = Rs[2] - Rs[1];
+            Gx = Gs[2] - Gs[1];
+            Bx = Bs[2] - Bs[1];
+
+            Ry = Rs[4] - Rs[3];
+            Gy = Gs[4] - Gs[3];
+            By = Bs[4] - Bs[3];
+
+
+
+            //calculate energy
+            int deltaX = Rx*Rx + Gx*Gx + Bx*Bx;
+            int deltaY = Ry*Ry + Gy*Gy + By*By;
+            int res = sqrt(deltaX + deltaY);
+
+            *grad->//got to do something w. this
+
+        }
+    }
+
+
+//int[]
+
 
     int Rx, Gx, Bx, Ry, Gy, By; //The difference in RGB values in the pixels above & below, and left & right
 
@@ -23,7 +92,11 @@ int main() {
     printf("Hello, World!");
 
     struct rgb_img *im;
-    create_img(&im, 3, 3); // do these values need to be hardcoded?
+    create_img(&im, 3, 4); // do these values need to be hardcoded?
+    read_in_img(&im, "3x4.bin");
+
+    uint8_t temp = get_pixel(im, 1, 2, 0);
+
 
 
 
